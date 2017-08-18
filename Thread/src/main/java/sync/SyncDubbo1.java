1410d2a033e40ebddd70cee945322749f477a7d8
qq1367212627 <1367212627@qq.com>
2017-08-18 15:08:00 +0800

    锁的重入问题

---
+++ Thread/src/main/java/sync/SyncDubbo1.java
@@ -0,0 +1,31 @@
+package sync;
+
+/**
+ * Created by lewis on 2017/8/18.
+ */
+public class SyncDubbo1 {
+    /**
+     * 锁的重入问题
+     * */
+    public synchronized void method1(){
+        System.out.println("method 1 ..");
+        method2();
+    }
+    public synchronized void method2(){
+
+        System.out.println("method 2 ..");
+        method3();
+    }
+    public synchronized void method3(){
+        System.out.println("method 3 ..");
+    }
+    public static void main(String[] args){
+        final SyncDubbo1 syncDubbo1 = new SyncDubbo1();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                syncDubbo1.method1();
+            }
+        }).start();
+    }
+}
