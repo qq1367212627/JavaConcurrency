eb1fb76fd2dae1d63ef07f62cac3c4165ec0f200
qq1367212627 <1367212627@qq.com>
2017-10-08 17:13:18 +0800

    netty拆包（通过特殊字符实现tcp字节的流拆包，在本例中 特殊字符为 '$_'

---
+++ multi09_netty/src/main/java/indi/lewis/netty/ende1/Client.java
@@ -0,0 +1,39 @@
+
+package indi.lewis.netty.ende1;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+public class Client {
+
+    public static void main(String[] agrs) throws InterruptedException {
+        EventLoopGroup group  = new NioEventLoopGroup();//
+        Bootstrap boot = new Bootstrap();//配置工具类
+        boot.group(group).//绑定线程组
+        channel(NioSocketChannel.class).//指定通信模式：客户端 NIO 模式
+        handler(new ChannelInitializer<SocketChannel>() {//
+            @Override
+            protected void initChannel(SocketChannel socketChannel) throws Exception {
+                ByteBuf buf = Unpooled.copiedBuffer("$_".getBytes());
+                socketChannel.pipeline().addLast(new DelimiterBasedFrameDecoder(1<<10,buf));
+                socketChannel.pipeline().addLast(new StringDecoder());
+                socketChannel.pipeline().addLast(new ClientHandler());
+
+            }
+        });
+        ChannelFuture cf = boot.connect("127.0.0.1",8000).sync();//
+        cf.channel().writeAndFlush(Unpooled.copiedBuffer("777777$_".getBytes()));//通过通道读写数据，从管道冲刷到服务端
+        cf.channel().writeAndFlush(Unpooled.copiedBuffer("8885555$_".getBytes()));//
+        cf.channel().closeFuture().sync();//阻塞
+        group.shutdownGracefully();//
+    }
+}
---
+++ multi09_netty/src/main/java/indi/lewis/netty/ende1/Server.java
@@ -0,0 +1,47 @@
+
+package indi.lewis.netty.ende1;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.DelimiterBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+public class Server {
+
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup pgroup = new NioEventLoopGroup();//处理客户端建立连接
+        EventLoopGroup cGroup = new NioEventLoopGroup();//处理客户端网络通信
+        //工具类，用于服务器通道的配置
+        ServerBootstrap bootstrap = new ServerBootstrap();//配置辅助类
+        bootstrap.group(pgroup,cGroup).//绑定线程组
+        channel(NioServerSocketChannel.class).//指定模式 ：服务器端 NIO模式
+        option(ChannelOption.SO_BACKLOG,1<<10).//设置tcp缓冲区（tcp握手，tcp内核）
+        option(ChannelOption.SO_SNDBUF,1<<15).//发送缓冲区大小
+        option(ChannelOption.SO_RCVBUF,1<<15).//接受缓冲区大小
+        option(ChannelOption.SO_KEEPALIVE,true).//是否保持连接
+        childHandler(new ChannelInitializer<SocketChannel>() {//
+           @Override
+            protected void initChannel(SocketChannel socketChannel) throws Exception {
+                ByteBuf buf = Unpooled.copiedBuffer("$_".getBytes());
+                socketChannel.pipeline().addLast(new DelimiterBasedFrameDecoder(1<<10,buf));
+                socketChannel.pipeline().addLast(new StringDecoder());
+                socketChannel.pipeline().addLast(new ServerHandler());//配置数据接受方法的处理
+            }
+        });
+
+
+        ChannelFuture cf = bootstrap.bind(8000).sync();//端口绑定
+        cf.channel().closeFuture().sync();// 阻塞
+        pgroup.shutdownGracefully();//释放资源
+        cGroup.shutdownGracefully();//
+    }
+
+}
